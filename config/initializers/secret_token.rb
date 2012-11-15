f18026f4930cfe3f07ac63eb4901d61305e1b620
bangarang <me@alexjrock.com>
2012-11-14 18:01:03 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simplifybest::Application.config.secret_token = '76495929b0f4414f87f09d8690cbdfe2abecd04376f8f2d81bcd5068681711947dec1fd368e2881a7acfb9315948c9bf402b9bb19772423abb2992f5a1c5cba4'
