fb605b6f902994b330d6cde332c73c66b3209791
bangarang <me@alexjrock.com>
2012-12-23 19:16:47 -0600

    changed precompile settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,3 +12,6 @@ class User < ActiveRecord::Base
 
   validates :email, :uniqueness => true
 end
+
+
+# user = User.new(:email => "alex@simplifybest.com", :password => "R3dnax3la", :password_confirmation => "R3dnax3la", :remember_me => "", :role_ids => "")
