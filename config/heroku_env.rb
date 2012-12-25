6acfa0eb9c09ab3e5c17b42ef71377724d91c1c6
bangarang <me@alexjrock.com>
2012-12-25 14:27:44 -0600

    ui changes

---
+++ config/heroku_env.rb
@@ -0,0 +1,8 @@
+# This file contains the ENV vars necessary to run the app locally.  
+# Some of these values are sensitive, and some are developer specific.
+#
+# DO NOT CHECK THIS FILE INTO VERSION CONTROL
+
+ENV["AWS_ACCESS_KEY_ID"] = 'AKIAIPUODYHAKV34GWOA'
+ENV["AWS_SECRET_ACCESS_KEY"] ='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'
+ENV["AWS_S3_BUCKET"] = 'simplifybesttest'
