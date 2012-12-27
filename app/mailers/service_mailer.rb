24f005317d8eae70aaa0534596d6a80b2037212e
bangarang <me@alexjrock.com>
2012-12-27 00:09:18 -0600

    Meters form

--- app/mailers/service_mailer.rb
+++ app/mailers/service_mailer.rb
@@ -1,6 +1,6 @@
 class ServiceMailer < ActionMailer::Base
   default :to => "alex@simplifybest.com"
-  default :from => "kcorxela@gmail.com"
+  default :from => "meter-reading@simplifybest.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -8,11 +8,11 @@ class ServiceMailer < ActionMailer::Base
   #   en.service_mailer.meters.subject
   #
   def meters(email_params, sent_at = Time.now)
-    @subject = email_params[:subject]
-    @sender = email_params[:address]
     @sent_on = sent_at
-    @body = email_params[:body]
     @name = email_params[:name]
-    mail(:subject => email_params[:subject])
+    @company = email_params[:company]
+    @sos = email_params[:sos]
+    @meter = email_params[:meter]
+    mail(:subject => "Meter Reading")
   end
 end
