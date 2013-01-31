ec8dbfb6285e5a648c944dad34a9a1f0a8a545bb
bangarang <me@alexjrock.com>
2013-01-31 14:54:41 -0600

    removed config.storage :fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,5 @@
 CarrierWave.configure do |config|
 	config.cache_dir = "#{Rails.root}/tmp/"
-	config.storage = :fog
 	config.permissions = 0666
 	config.fog_credentials = {
 		:provider 				=> 'AWS',
