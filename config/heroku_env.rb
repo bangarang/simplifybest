a9611eaee688c3b31482488a788a2c9ee8eb22e2
bangarang <me@alexjrock.com>
2013-01-31 15:15:18 -0600

    testing sitemap config

--- config/heroku_env.rb
+++ config/heroku_env.rb
@@ -8,5 +8,3 @@ ENV["AWS_SECRET_ACCESS_KEY"] ='rlyOk+TdEywh27oh6IFfJOWwIcTx9LM9suKobQtG'
 ENV["AWS_S3_BUCKET"] = 'assets.simplifybest.com'
 ENV["MANDRILL_USERNAME"] = 'alex@simplifybest.com'
 ENV["MANDRILL_API_KEY"] = '578e1538-3b54-4848-9cad-ae4392bc0d34'
-ENV['GMAIL_USER'] = "kcorxela@gmail.com"
-ENV['GMAIL_PASSWORD'] = "Captain's hat"
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
 		:aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],
 		:aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"]
 	}
+	config.storage = :fog
 	config.fog_directory = ENV["AWS_S3_BUCKET"],
 	config.asset_host     = 'http://assets.simplifybest.com' 
 end
