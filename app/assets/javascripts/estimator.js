c0f6b9d8fefa32e4910bf2e0febc32c0fe901720
bangarang <me@alexjrock.com>
2013-05-21 22:12:08 -0500

    estimator.js to estimator.js.coffee

--- app/assets/javascripts/estimator.js
+++ app/assets/javascripts/estimator.js
@@ -1,77 +1,61 @@
-angular.module('estimator', []);
+app = angular.module("estimator", [])
 
+@EstimatorCtrl["$scope"
+($scope) ->
+  $scope.tonercost = 0
+  $scope.toneryield = 1
+  $scope.tonercpc = 0
+  $scope.drumcost = 0
+  $scope.drumyield = 1
+  $scope.drumcpc = 0
+  $scope.pmcost = 0
+  $scope.pmyield = 1
+  $scope.pmcpc = 0
+  $scope.totalcpc = 0
+  $scope.tonerCPC = ->
+    $scope.tonercpc = $scope.tonercost / $scope.toneryield
+    $scope.tonercpc
 
+  $scope.noToner = ->
+    if $scope.toner
+      $scope.tonercost = ""
+      $scope.toneryield = ""
+      $scope.tonercpc = ""
+    else
+      $scope.tonercost = 0
+      $scope.toneryield = 1
+      $scope.tonercpc = 0
 
-function EstimatorCtrl($scope) {
-  $scope.tonercost = 0;
-  $scope.toneryield = 1;
-  $scope.tonercpc = 0;
-  $scope.drumcost = 0;
-  $scope.drumyield = 1;
-  $scope.drumcpc = 0;
-  $scope.pmcost = 0;
-  $scope.pmyield = 1;
-  $scope.pmcpc = 0;
-  $scope.totalcpc = 0;
+  $scope.drumCPC = ->
+    $scope.drumcpc = $scope.drumcost / $scope.drumyield
+    $scope.drumcpc
 
-  $scope.tonerCPC = function() {
-    $scope.tonercpc = $scope.tonercost / $scope.toneryield; 
-    return $scope.tonercpc;
-  }
+  $scope.noDrum = ->
+    if $scope.drum
+      $scope.drumcost = ""
+      $scope.drumyield = ""
+      $scope.drumcpc = ""
+    else
+      $scope.drumcost = 0
+      $scope.drumyield = 1
+      $scope.drumcpc = 0
 
-  $scope.noToner = function() {
-    if($scope.toner) {
-      $scope.tonercost = '';
-      $scope.toneryield = '';
-      $scope.tonercpc = '';
-    }
-    else {
-      $scope.tonercost = 0;
-      $scope.toneryield = 1;
-      $scope.tonercpc = 0;
-    }
-  }
+  $scope.pmCPC = ->
+    $scope.pmcpc = $scope.pmcost / $scope.pmyield
+    $scope.pmcpc
 
-  $scope.drumCPC = function() {
-    $scope.drumcpc =$scope.drumcost / $scope.drumyield; 
-    return $scope.drumcpc;
-  }
+  $scope.noPm = ->
+    if $scope.pm
+      $scope.pmcost = ""
+      $scope.pmyield = ""
+      $scope.pmcpc = ""
+    else
+      $scope.pmcost = 0
+      $scope.pmyield = 1
+      $scope.pmcpc = 0
 
-  $scope.noDrum = function() {
-    if($scope.drum) {
-      $scope.drumcost = '';
-      $scope.drumyield = '';
-      $scope.drumcpc = '';
-    }
-    else {
-      $scope.drumcost = 0;
-      $scope.drumyield = 1;
-      $scope.drumcpc = 0;
-    }
-  }
+  $scope.totalCPC = ->
+    $scope.totalcpc = $scope.tonercpc + $scope.pmcpc + $scope.drumcpc
+    $scope.totalcpc
 
-  $scope.pmCPC = function() {
-    $scope.pmcpc = $scope.pmcost / $scope.pmyield; 
-    return $scope.pmcpc;
-  }
-
-  $scope.noPm = function() {
-    if($scope.pm) {
-      $scope.pmcost = '';
-      $scope.pmyield = '';
-      $scope.pmcpc = '';
-    }
-    else {
-      $scope.pmcost = 0;
-      $scope.pmyield = 1;
-      $scope.pmcpc = 0;
-    }
-  }
-
-  $scope.totalCPC = function() {
-    $scope.totalcpc = $scope.tonercpc + $scope.pmcpc + $scope.drumcpc; 
-    return $scope.totalcpc;
-  }
-
-}
-
+]
