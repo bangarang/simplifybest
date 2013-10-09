f0b5d05e1bf46b54463fc359958c95b043e9d233
bangarang <me@alexjrock.com>
2013-10-09 08:29:10 -0500

    brand

--- app/controllers/admin_controller.rb
+++
@@ -1,8 +0,0 @@
-class AdminController < ApplicationController
-  before_filter :authenticate_user!
-  def index
-  	@users = User.all
-  	@featured_pictures = FeaturedPicture.all
-  	@messages = Message.all
-  end
-end
