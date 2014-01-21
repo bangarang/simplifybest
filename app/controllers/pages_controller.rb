65d91b3dcb18999190a29af6ec2bbbbcf49e0f5d
bangarang <me@alexjrock.com>
2014-01-21 16:39:28 -0600

    Groups

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -29,22 +29,22 @@ class PagesController < ApplicationController
 
   def printers
     @group = "Printer"
-    @categories = Category.joins(:products).where(:products => {:printer => true})
+    @products = Product.where(:products => {:printer => true})
     render "pages/groups"
   end 
   def workgroup
     @group = "Workgroup"
-    @categories = Category.joins(:products).where(:products => {:workgroup => true})
+    @products = Product.where(:products => {:workgroup => true})
     render "pages/groups"
   end 
   def smalloffice
     @group = "Small Office"
-    @categories = Category.joins(:products).where(:products => {:smalloffice => true})
+    @products = Product.where(:products => {:smalloffice => true})
     render "pages/groups"
   end 
   def enterprise
     @group = "Enterprise"
-    @categories = Category.joins(:products).where(:products => {:enterprise => true})
+    @products = Product.where(:products => {:enterprise => true})
     render "pages/groups"
   end 
 end
