8ba3aae444b19310a560068fed8fbab47569a931
bangarang <me@alexjrock.com>
2014-04-04 10:14:29 -0500

    No Drop Shadow. UI Changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Simplifybest::Application.configure do
   }
   
   CarrierWave.configure do |config|
-    config.asset_host = 'http://assets.simplifybest.com'
+    config.asset_host = 'd2pljus72idd74.cloudfront.net'
   end
-  config.asset_host = 'http://assets.simplifybest.com'
+  config.asset_host = 'd2pljus72idd74.cloudfront.net'
 end
