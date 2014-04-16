d131ae54581cbdf36887617f84efc920acab205e
bangarang <me@alexjrock.com>
2014-04-16 17:21:48 -0500

    aws  stuff

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -4,5 +4,6 @@ if Rails.env.production? && defined?(AssetSync)
     config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
     config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
     config.fog_directory = ENV['FOG_DIRECTORY']
+    config.fog_region = ENV['S3_REGION']
   end
 end
