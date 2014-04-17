260185cc12ce2d5ca08929258f69b419c8459116
bangarang <me@alexjrock.com>
2014-04-16 22:52:52 -0500

    stuff

--- db/migrate/20140417024807_stuffthungs.rb
+++ db/migrate/20140417024807_stuffthungs.rb
@@ -1,6 +1,6 @@
 class Stuffthungs < ActiveRecord::Migration
   def change
-    remove_column :products, :pic_url, :string
+    remove_column :products, :pic_url
     add_column :product_pictures, :pic_url, :string
   end
 end
